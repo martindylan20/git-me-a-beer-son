57a17fb3ca93f2a8dea579756b634252bcac5712
Dylan Martin <143762725+pad0may@users.noreply.github.com>
2026-09-22 08:28:38 -0700

    (GP-2.1): Program Can Initialize Repository
    Added initialization feature to Git.java

--- Git.java
+++ Git.java
@@ -1,5 +1,31 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Git {
     public static void main(String[] args) {
-        
+        init();
+    }
+
+    public static void init() {
+        Path objPath = Paths.get("./git/objects");
+        Path indexPath = Paths.get("./git/index");
+        Path headPath = Paths.get("./git/HEAD");
+
+        try {
+            if (Files.exists(objPath) && Files.exists(indexPath) && Files.exists(headPath)) {
+                System.out.println("Git Repository Already Exists");
+                return;
+            } if (!Files.exists(objPath)) {
+                Files.createDirectories(objPath);
+            } if (!Files.exists(indexPath)) {
+                Files.createFile(indexPath);
+            } if (!Files.exists(headPath)) {
+                Files.createFile(headPath);
+            }
+            System.out.println("Git Repository Created");
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
